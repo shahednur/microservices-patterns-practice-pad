27d4efc4873b78d211d8472797a665bd073a495b
shahednur <shahednurctg@gmail.com>
2025-09-01 22:50:53 +0600

    Added order repository into order-service

--- order-service/src/main/java/com/shahed/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/shahed/orderservice/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.shahed.orderservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.shahed.orderservice.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    Optional<Order> findByOrderNumber(String orderNumber);
 }
